49c03828c3c2f08d31c2b280130d071c21f5e79e
Successdanesy <successdanesy@gmail.com>
2025-09-15 14:25:29 +0100

    App file is clean and component structure is utitlized

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Navbar from "./components/Navbar"
 import Hero from "./components/Hero"
 import HomeCards from "./components/HomeCards"
 import JobListings from "./components/JobListings.jsx"
-import JobListing from "./components/JobListing.jsx"
+import ViewAllJobs from "./components/ViewAllJobs.jsx"
 
 const App = () => {
     return (
@@ -11,14 +11,8 @@ const App = () => {
             <Hero />
             <HomeCards />
             <JobListings />
+            <ViewAllJobs />
 
-            <section className="m-auto max-w-lg my-10 px-6">
-                <a
-                    href="jobs.html"
-                    className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
-                >View All Jobs</a
-                >
-            </section>
         </>
     )
 }
