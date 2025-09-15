334b7f95adce4628cd1e502abc56ebcd67120425
Successdanesy <successdanesy@gmail.com>
2025-09-15 16:36:38 +0100

    Router works, added navbar and hero to main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import HomeCards from "./components/HomeCards"
-import JobListings from "./components/JobListings.jsx"
-import ViewAllJobs from "./components/ViewAllJobs.jsx"
+import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider,} from "react-router-dom"
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/HomePage";
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path="/" element={<MainLayout />}>
+            <Route index element = {<HomePage />} />
+        </Route>
+    )
+);
 
 const App = () => {
-    return (
-        <>
-            <Navbar />
-            <Hero />
-            <HomeCards />
-            <JobListings />
-            <ViewAllJobs />
-        </>
-    )
-}
-export default App
+    return <RouterProvider router={router} />;
+};
+export default App;
 
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import Hero from "../components/Hero";
+import HomeCards from "../components/HomeCards";
+
+
+const Homepage = () => {
+    return (
+        <>
+        <Hero />
+            <HomeCards />
+        </>
+    );
+};
+export default Homepage
