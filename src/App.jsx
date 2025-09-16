bf8a89afe74d2a6ea082b36f458a097ba32a26f3
Successdanesy <successdanesy@gmail.com>
2025-09-16 16:48:44 +0100

    Added a Not found page for layouts i haven't created yet

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider,} f
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage.jsx";
 
 
 const router = createBrowserRouter(
@@ -9,6 +10,7 @@ const router = createBrowserRouter(
         <Route path="/" element={<MainLayout />}>
             <Route index element = {<HomePage />} />
             <Route path="/jobs" element={<JobsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
         </Route>
     )
 );
