7b276f49143a8bf5ed9b591c2c9466803714e897
Successdanesy <successdanesy@gmail.com>
2025-09-22 12:43:37 +0100

    Setup Proxy on backend (Fetch on render)

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,15 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-      tailwindcss(),
-      react()],
-})
+  plugins: [tailwindcss(), react()],
+    server: {
+      port: 5173,
+        proxy: {
+          '/api':  {
+              target: 'http://localhost:8000',
+              changeOrigin: true,
+              rewrite: (path) => path.replace(/^\/api/, ''),
+          },
+        },
+    },
+});
